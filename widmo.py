1677427fe4fcd39821f9e0b53242936fa60bb8fc
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-03-01 11:48:01 +0100

    initial agilent read method

--- widmo.py
+++ widmo.py
@@ -12,8 +12,10 @@ class Widmo:
         self.piki = None
         self.widmo = None
         pass
-    def bruker(self, fid):
+    def agilent_read(self, fid):
         headers, fids = read_agilent_fid(fid)
-        pass
+        ft_rl = np.fft.fft(np.real(fids[0]))
+        ft_im = np.fft.fft(np.imag(fids[0]))
+        self.widmo = [(i*i + j*j) for i, j in zip(np.real(ft_rl[:len(ft_rl)//2]), np.imag(ft_im[:len(ft_im)//2]))]
     def integracja(self):
         pass
