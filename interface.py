12d5969ede45a900d524841beb4d86acc6868402
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-03-01 19:36:37 +0100

    basic frame for drawing

--- interface.py
+++ interface.py
@@ -1,24 +1,33 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtGui import QPainter, QColor, QPixmap
 from PyQt6.QtCore import Qt
 
 class spectrum_painter(QWidget):
-    def __init__(self):
+    def __init__(self, size, margins):
         super().__init__()
-
-    def paintEvent(self, event):
+        self.painter_size = {
+            'w' : size['w']-margins['right']-margins['left'], 
+            'h' : size['h']-margins['top']-margins['bottom']
+        }
+        self.margins = margins
+        self.pixmap = QPixmap(self.painter_size['w'], self.painter_size['h'])
+    
+    def paintEvent(self, margin):
         painter = QPainter(self)
         #painter.setPen(QColor(0, 0, 255))  # color
-        painter.drawLine(50, 100, 200, 100)
+        painter.drawRect(self.margins['left'], self.margins['top'], self.painter_size['w'], self.painter_size['h'])
+        painter.end()
+        painter.drawPixmap(self.margins['left'], self.margins['top'], self.pixmap) 
 
 class window(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.setGeometry(200, 200, 800, 400) #placement, windowsize
+        size = {'w':800,'h':400}
+        margins = {'top':80, 'left':80, 'bottom':10, 'right':10}
+        self.setGeometry(200, 200, size['w'], size['h']) #placement, windowsize
         self.setWindowTitle("Open NMR") 
-        self.central_widget = spectrum_painter() 
+        self.central_widget = spectrum_painter(size, margins) 
         self.setCentralWidget(self.central_widget)
 
 if __name__ == "__main__":
