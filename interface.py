e39ed280dd5b58fd139d8c4a667ee7f2113c6572
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-03-02 10:14:42 +0100

    no downsampling working

--- interface.py
+++ interface.py
@@ -1,23 +1,28 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt6.QtGui import QPainter, QColor, QPixmap, QPolygon
-from PyQt6.QtCore import QPoint
+from PyQt6.QtGui import QPainter, QColor, QPixmap, QPolygon, QPen
+from PyQt6.QtCore import QPointF, Qt
 import numpy as np
 import noise
 from math import floor
+from readingfids import *
 
 def fast_data(data, width, height):
-    # this is very primitive and fast
-    # the value closest to a pixel is chosen
-    result = []
+    #normalize
     ymax = max(data[:,1])
     ymin = min(data[:,1])
-    print(ymin, ymax)
-    for i in range(width):
-        y = data[floor(len(data)*i/width), :][1]
-        y = floor((y-ymin)/(ymax-ymin)*height)
-        result.append((i,int(y)))
-    return result
+    print(data[:,1].shape)
+    print(ymax, ymin)
+    data[:,1] = (data[:,1]-ymin)/(ymax-ymin)
+    # this is downsampling data to pixels
+    downsampled  = data * (width, height)
+    # for i in range(len(downsampled)):
+    #     pt = downsampled.pop(0)
+    #     if result: 
+    #         if result[-1] != pt : result.append(pt)
+    # mask = np.append([True], np.any(np.diff(downsampled, axis=0), axis=1))
+    # result = downsampled[mask]
+    return downsampled
 
 class spectrum_painter(QWidget):
     def __init__(self, margins, data):
@@ -29,6 +34,11 @@ class spectrum_painter(QWidget):
         
 
     def paintEvent(self, event):
+        #settings
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        #window size
         rect = self.rect()
         size = {
             'w' : rect.topRight().x()-rect.topLeft().x(),
@@ -38,14 +48,19 @@ class spectrum_painter(QWidget):
             'w' : size['w']-self.margins['right']-self.margins['left'], 
             'h' : size['h']-self.margins['top']-self.margins['bottom']
         }
-        self.resampled = fast_data(data, self.painter_size['w'], self.painter_size['h'])
-        painter = QPainter(self)
         painter.translate(self.margins['left'], self.margins['top'])
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        print(event)
-        #painter.setPen(QColor(0, 0, 255))  # color
+
+
+        #drawing plot
         painter.drawRect(0, 0, self.painter_size['w'], self.painter_size['h'])
-        painter.drawPolyline(QPolygon([QPoint(x,y) for (x,y) in self.resampled]))
+        self.resampled = fast_data(data, self.painter_size['w'], self.painter_size['h'])
+        print(event)
+        for i in range(len(self.resampled)-1): 
+            p1 = QPointF(self.resampled[i][0], self.resampled[i][1])
+            p2 = QPointF(self.resampled[i+1][0], self.resampled[i+1][1])
+            painter.drawLine(p1,p2)
+        #painter.drawPolyline(QPolygon([QPointF(x,y) for (x,y) in self.resampled]))
+        
         painter.end()
 
 class window(QMainWindow):
